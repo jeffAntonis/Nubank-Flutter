15961b978869b74c53f9e43ab16d1e4aa3e0f85f
jeffAntonis <antunis356@gmail.com>
2019-08-19 00:44:16 -0300

    Implementado animação de opacidade

--- lib/components/Tabs.dart
+++ lib/components/Tabs.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+
+import 'package:nubank/models/AppState.dart';
 
 import 'package:nubank/models/Item.dart';
 
@@ -16,73 +19,76 @@ class _TabsState extends State<Tabs> {
     Item(title: 'Bloquear cartão', codIcon: 59543),
   ];
 
-  final bool _visible = true;
-
   @override
   Widget build(BuildContext context) {
-    return Positioned(
-      bottom: 0,
-      left: 0,
-      right: 0,
-      child: AnimatedOpacity(
-        opacity: _visible ? 1.0 : 0.0,
-        duration: Duration(milliseconds: 200),
-        child: Container(
-          height: 100,
-          margin: EdgeInsets.only(
-            top: 20,
-          ),
-          child: ListView.builder(            
-            padding: EdgeInsets.only(
-              left: 10,
-              right: 20,
-            ),
-            scrollDirection: Axis.horizontal,
-            itemCount: items.length,
-            itemBuilder: (context, index) {
-              return Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Container(
-                  width: 100,
-                  height: 100,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(3),
-                    color: Color.fromRGBO(255, 255, 255, 0.2),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.only(
-                      left: 5,
-                      top: 5,
-                      bottom: 5,
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Icon(
-                          IconData(
-                            items[index].codIcon,
-                            fontFamily: 'MaterialIcons',
-                          ),
-                          color: Colors.white,
-                          size: 20,
-                        ),
-                        Text(
-                          items[index].title,
-                          style: TextStyle(
-                            fontSize: 13,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
+    return StoreConnector<AppState, AppState>(
+      converter: (store) => store.state,
+      builder: (context, state) {
+        return Positioned(
+          bottom: 0,
+          left: 0,
+          right: 0,
+          child: AnimatedOpacity(
+            opacity: !state.animated ? 1.0 : 0.0,
+            duration: Duration(milliseconds: 300),
+            child: Container(
+              height: 100,
+              margin: EdgeInsets.only(
+                top: 20,
+              ),
+              child: ListView.builder(
+                padding: EdgeInsets.only(
+                  left: 10,
+                  right: 20,
                 ),
-              );
-            },
+                scrollDirection: Axis.horizontal,
+                itemCount: items.length,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Container(
+                      width: 100,
+                      height: 100,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(3),
+                        color: Color.fromRGBO(255, 255, 255, 0.2),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.only(
+                          left: 5,
+                          top: 5,
+                          bottom: 5,
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Icon(
+                              IconData(
+                                items[index].codIcon,
+                                fontFamily: 'MaterialIcons',
+                              ),
+                              color: Colors.white,
+                              size: 20,
+                            ),
+                            Text(
+                              items[index].title,
+                              style: TextStyle(
+                                fontSize: 13,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
